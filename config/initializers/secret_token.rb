12b49150dab734949d26b4d6e1284d7d53d144e7
whitenhiemer <bwoodwar@gmail.com>
2013-06-12 17:39:09 +0000

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpsWorksRails::Application.config.secret_token = 'ed24432728ee9058c6483fa3cb1fac924189d465a4009ac21492791ce183d6ac5ea97ec29e1f0d984c88b145f260c7ff39882dcbdf60d0d1add3019229a14213'
